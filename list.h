ebe095584b7e79cd95d276b1b6c7838134814c19
Stefan <stefan@Tasks-Deb>
2017-12-04 12:20:12 +0200

    adaugat explicatie pt scopul unui lock

--- list.h
+++ list.h
@@ -10,10 +10,20 @@ typedef struct node
     Tprint print;
     int val;
     struct node *next;
-    //acest lock este redundant
+    
+    // acest lock este redundant
     // daca ar fi posibila promovarea de la read la write,
     // as putea folosi acest lock pentru a permite accesul read
-    // la lista in timp ce fac operatii de write
+    // la lista in timp ce fac operatii limitate de write
+
+    // aceste lockuri sunt folosite in momentul in care iteram prin
+    // lista pentru a face un fel de bariera care sa blocheze ca un
+    // alt thread sa depaseasca starea la care ne aflam in acest
+    // moment
+    
+    // in situatia write, serializeaza accesul la lista, in situatia
+    // read, asigura ca valoarea next nu e modificata pana ca ajungem
+    // la nodul respectiv
     pthread_rwlock_t lock;
 } Tnode;
 
